4d75b3655d7acdd2a3563c279b53dc74ebbd8312
gevgev <gevgev@yahoo.com>
2016-05-16 11:41:12 -0700

    enabled http/https to nat from private cidr

--- security-groups.tf
+++ security-groups.tf
@@ -130,6 +130,20 @@ resource "aws_security_group" "nat" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port  = 80
+    to_port    = 80
+    protocol   = "tcp"
+    cidr_blocks = ["10.198.1.0/24"]
+  }
+
+  ingress {
+    from_port  = 443
+    to_port    = 443
+    protocol   = "tcp"
+    cidr_blocks = ["10.198.1.0/24"]
+  }
+
   egress {
     from_port   = -1
     to_port     = -1
